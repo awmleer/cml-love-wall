86e7c93e3203cac3b8053eaa79f6bf5684b54304
haoguangbo <haoguangbo@yeah.net>
2016-02-29 23:21:11 +0800

    一些小调整

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@ app.controller("ctrlindex", function ($scope,$http) {
     $scope.genderfilter = 'girl';
     //TODO http获取items
     $http({
-        url: 'http://127.0.0.1:3000/items',
+        url: 'http://120.27.121.63:3000/items',
         method: 'get',
     }).success(function (data) {
         $scope.items = data;
